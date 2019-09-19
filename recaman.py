accee392c2b1ab14e2538d7c40fde4b1f1080f4a
Koketso B. Mangwale <bmangwale@gmail.com>
2019-09-19 16:28:37 +0200

    Update recaman.py

--- recaman.py
+++ recaman.py
@@ -1,2 +1,10 @@
 import turtle #python module that allows use of turtles
 
+window = turtle.Screen()
+kb = turtle.Turtle()
+
+kb.forward(50)#move forward by 50 units
+kb.left(90)#turn left 90 degrees
+kb.forward(30)#move forward by 30 units
+
+window.mainloop()
